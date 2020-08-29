f389a530d3e40782ef9ee9d270b98e51073f3bcd
Luan Jesus <luan.todos@gmail.com>
2020-08-29 20:39:39 -0300

    SPA aplication hello world.

---
+++ spa/src/pages/Dashboard/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function Dashboard() {
+  return (
+    <Container>
+      <div>Eaei</div>
+    </Container>
+  );
+}
+
+export default Dashboard;
---
+++ spa/src/pages/Dashboard/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #f2341d;
+  padding: 20px;
+`;
